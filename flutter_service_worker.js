571c22d4898868fa788579be688cc390a5f4c958
Frank <FrankEmail>
2022-10-21 13:47:51 +0800

    0.0.4

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "4a6b44b27247639df2d59b98f133fe9f",
-"index.html": "b85d5260476dd4d944281726dd62eb43",
-"/": "b85d5260476dd4d944281726dd62eb43",
-"main.dart.js": "a52243990fd53f54b225ce18fc5928ec",
+"index.html": "f28c078e6ac652f66fbeb33cc37c7d3f",
+"/": "f28c078e6ac652f66fbeb33cc37c7d3f",
+"main.dart.js": "00bf2b2f3a6714d7a2478ff57f12b157",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
